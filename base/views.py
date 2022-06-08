dafff3004f5944fb5f5a2ac2c95a47716bbf704f
alex-budko <xalex.budko@gmail.com>
2022-06-08 19:45:44 -0400

    implemented the participants

--- base/views.py
+++ base/views.py
@@ -1,3 +1,4 @@
+from tkinter.font import ROMAN
 from django.db.models import Q
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
@@ -6,7 +7,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 
@@ -74,6 +75,15 @@ def room(request, pk):
     room = Room.objects.get(id=pk)
     #filters by time created in descending order
     room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+        return redirect('room', pk=room.id)
+
     context = {'room': room, 'room_messages':room_messages}
     return render(request, 'base/room.html', context)
 
