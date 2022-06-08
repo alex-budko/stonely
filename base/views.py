510048d7fbfb16b0662cbcf0307aa856be55ea89
alex-budko <xalex.budko@gmail.com>
2022-06-07 22:53:48 -0400

    implemented add + update form

--- base/views.py
+++ base/views.py
@@ -1,18 +1,49 @@
-from django.shortcuts import render
+from multiprocessing import context
+from django.shortcuts import render, redirect
+from .models import Room
+from .forms import RoomForm
 
-rooms = [
-    {'id': 1, 'name': 'Lets Learn Python'},
-    {'id': 2, 'name': 'Lets Learn Django'},
-    {'id': 3, 'name': 'Lets Learn Go'},
-]
+# rooms = [
+#     {'id': 1, 'name': 'Lets Learn Python'},
+#     {'id': 2, 'name': 'Lets Learn Django'},
+#     {'id': 3, 'name': 'Lets Learn Go'},
+# ]
+
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    form = RoomForm()
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    
