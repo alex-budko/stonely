d982d92412a03122c953d9c1d552084ac335fa09
alex-budko <xalex.budko@gmail.com>
2022-06-07 20:51:47 -0400

    Initialized repository for `Stonely`, a study app

---
+++ base/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+rooms = [
+    {'id': 1, 'name': 'Lets Learn Python'},
+    {'id': 2, 'name': 'Lets Learn Django'},
+    {'id': 3, 'name': 'Lets Learn Go'},
+]
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
