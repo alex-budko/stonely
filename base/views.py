8be691370d684a2b3450c3d14e3fcd81bd760489
alex-budko <xalex.budko@gmail.com>
2022-06-08 17:02:45 -0400

    user authentication

--- base/views.py
+++ base/views.py
@@ -1,6 +1,9 @@
-from multiprocessing import context
+from django.db.models import Q
 from django.shortcuts import render, redirect
-from .models import Room
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from .models import Room, Topic
 from .forms import RoomForm
 
 # rooms = [
@@ -8,10 +11,36 @@ from .forms import RoomForm
 #     {'id': 2, 'name': 'Lets Learn Django'},
 #     {'id': 3, 'name': 'Lets Learn Go'},
 # ]
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username OR password does not exist')
+    context = {}
+    return render(request, 'base/login_register.html', context)
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    topics = Topic.objects.all()
+
+    rooms = Room.objects.filter(Q(topic__name__icontains = q) 
+    | Q(name__icontains = q) | Q(description__icontains = q))
+
+    room_count = rooms.count()
+    context = {'rooms': rooms, 'topics': topics, 'room_count':room_count}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
