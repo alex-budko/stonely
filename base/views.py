e707f29fd3b6bfb3d472bcbe0d15bb79c54e526c
alex-budko <xalex.budko@gmail.com>
2022-06-08 21:05:02 -0400

    implemented activity feed

--- base/views.py
+++ base/views.py
@@ -67,14 +67,14 @@ def home(request):
     rooms = Room.objects.filter(Q(topic__name__icontains = q) 
     | Q(name__icontains = q) | Q(description__icontains = q))
 
+    room_messages = Message.objects.all()
     room_count = rooms.count()
-    context = {'rooms': rooms, 'topics': topics, 'room_count':room_count}
+    context = {'rooms': rooms, 'topics': topics, 'room_count':room_count, 'room_messages':room_messages}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    #filters by time created in descending order
-    room_messages = room.message_set.all().order_by('-created')
+    room_messages = room.message_set.all()
     participants = room.participants.all()
     if request.method == 'POST':
         message = Message.objects.create(
@@ -82,9 +82,10 @@ def room(request, pk):
             room = room,
             body = request.POST.get('body')
         )
+        room.participants.add(request.user)
         return redirect('room', pk=room.id)
 
-    context = {'room': room, 'room_messages':room_messages}
+    context = {'room': room, 'room_messages':room_messages, 'participants':participants}
     return render(request, 'base/room.html', context)
 
 @login_required(login_url='login')
@@ -127,3 +128,15 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj': room})
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user :
+        return HttpResponse('You do not have this permission')
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('room', message.room.id)
+    return render(request, 'base/delete.html', {'obj': message})
