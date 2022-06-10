29f185e85ae8d2abdd2873faa9d8238e415d9886
alex-budko <xalex.budko@gmail.com>
2022-06-10 17:11:20 -0400

    integrated custom user model

--- base/forms.py
+++ base/forms.py
@@ -1,15 +1,19 @@
 from importlib.abc import ExecutionLoader
 from django.forms import ModelForm
-from .models import Room
-from django.contrib.auth.models import User
-
+from .models import Room, User
+from django.contrib.auth.forms import UserCreationForm
 class RoomForm(ModelForm):
     class Meta:
         model = Room
         fields = '__all__'
         exclude = ['host' , 'participants']
 
+class MyUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['name', 'username', 'email', 'password1', 'password2']
+
 class UserForm(ModelForm):
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['name', 'username', 'email', 'bio', 'avatar']
--- stonely/settings.py
+++ stonely/settings.py
@@ -30,6 +30,8 @@ INSTALLED_APPS = [
     'corsheaders',
 ]
 
+AUTH_USER_MODEL = 'base.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     "corsheaders.middleware.CorsMiddleware",
@@ -112,6 +114,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+MEDIA_URL = '/images/'
+MEDIA_ROOT = BASE_DIR / 'static/images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
