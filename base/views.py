4fe2dae502b49d42dbc5d436bf3780ad3c3634be
alex-budko <xalex.budko@gmail.com>
2022-06-09 21:53:09 -0400

    edited user account page

--- base/views.py
+++ base/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from tkinter.font import ROMAN
 from django.db.models import Q
 from django.http import HttpResponse
@@ -8,7 +9,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 
 
 
@@ -160,3 +161,17 @@ def deleteMessage(request, pk):
         message.delete()
         return redirect('room', message.room.id)
     return render(request, 'base/delete.html', {'obj': message})
+
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+
+    context = {'form': form}
+    return render(request, 'base/update-user.html', context)
